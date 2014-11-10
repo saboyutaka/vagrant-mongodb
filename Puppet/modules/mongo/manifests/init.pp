01ee1cefae7abfd4837c30f3bf1bfe2f1cdca84f
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2014-11-11 00:02:10 +0900

    Add manifest about mongodb.conf that allow crient access.

--- Puppet/modules/mongo/manifests/init.pp
+++ Puppet/modules/mongo/manifests/init.pp
@@ -3,7 +3,7 @@ class mongo {
 	package {"mongodb":
 		ensure => installed
 	}
-	
+
 	package {"mongodb-server":
 		ensure => installed
 	}
@@ -12,6 +12,18 @@ class mongo {
 		ensure => running,
 		enable => true,
 		require => Package["mongodb-server"]
-	}	
+	}
 
+  file { "/etc/mongodb.conf" :
+      content => "
+        bind_ip = All
+        port = 27017
+        fork = true
+        pidfilepath = /var/run/mongodb/mongodb.pid
+        logpath = /var/log/mongodb/mongodb.log
+        dbpath =/var/lib/mongodb
+        journal = true
+        nohttpinterface = false
+      "
+  }
 }
